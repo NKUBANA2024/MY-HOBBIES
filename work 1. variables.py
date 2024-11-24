1271d55721b23ba416a784cc3a2b11de431e3992
NKUBANA2024 <nkubanajean@gmail.com>
2024-11-24 19:59:49 +0200

    Create work 1. variables.py

---
+++ work 1. variables.py
@@ -0,0 +1,7 @@
+annuaire = {"jado":"0788467452", 
+            "mamy": "0788761458", "jados": "0788467488",
+            "jados_keza": ["078844444", "078899999"], "akimana":
+                "07887545454"}
+print(annuaire["jado"])
+print(annuaire["jados_keza"])
+sorted(annuaire.items(), key=lambda t: t[0])
