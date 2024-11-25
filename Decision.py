43e69747b852ac19a0d0ac4fc7a5cabc3d3862af
NKUBANA2024 <nkubanajean@gmail.com>
2024-11-25 23:17:13 +0200

    Update Decision.py

--- Decision.py
+++ Decision.py
@@ -2,3 +2,6 @@ fruits = ["Pomme", "avocat", "ananas"]
 len(fruits) > 5
 'citron' in fruits
 'avocat' in fruits
+annuaire = {'kevin': '0606060606', 'radji': '06466548454', 'sos': '911'}
+"kevin" in annuaire
+"kevn" in annuaire
