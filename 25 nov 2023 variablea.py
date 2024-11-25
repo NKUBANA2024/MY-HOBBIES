9d8131ed2f9e8a120c8c8f164496ce5a22310fe1
NKUBANA2024 <nkubanajean@gmail.com>
2024-11-25 23:08:17 +0200

    Update 25 nov 2023 variablea.py

--- 25 nov 2023 variablea.py
+++ 25 nov 2023 variablea.py
@@ -7,3 +7,13 @@ prenom = "Credo Anissa"
 age = 6
 message = f"je suis {nom}  {prenom} et j'ai {age +1} ans"
 print(message)
+print(prenom)
+print(age)
+print(prenom)
+age = int(age)
+print(age)
+
+#
+username = input("Quel est ton nom d'utilisateur: mugeni1 ")
+password = input("Quel est ton mot de passe: 123")
+print("Bonjour ",username, " Votre mot de passe n'est pas sécurisé pour le moment. Je peux le lire, c'est : ", password)
